d3c3acf6350131124a058d00807f7f46ffbde5db
elviritica <e.aviles.s@gmail.com>
2024-03-30 13:15:38 +0100

    terminado estilado

--- src/pages/movement-list/components/account-info.component.tsx
+++ src/pages/movement-list/components/account-info.component.tsx
@@ -2,27 +2,25 @@ import { AccountInfoVM } from "../account-info.vm";
 import classes from "./account-info.component.module.css";
 
 interface Props {
-    accountInfo: AccountInfoVM;
+  accountInfo: AccountInfoVM;
 }
 
-export const AccountInfoComponent : React.FC<Props> = (props) => {
-    const {accountInfo} = props;
+export const AccountInfoComponent: React.FC<Props> = (props) => {
+  const { accountInfo } = props;
 
-    return(
-      <div className={classes.root}>
-        <div className={classes.headerContainer}>
-          <h1>Saldos y Últimos movimientos</h1>
-          <div>
-            <p>SALDO DISPONIBLE</p>
-            <p>{accountInfo.balance}€</p>
-          </div>
-        </div>
-        <div>
-          <p>Alias</p>
-            <p>{accountInfo.name}</p>
-          <p>IBAN</p>
-            <p>{accountInfo.iban}</p>
+  return (
+    <div className={classes.root}>
+      <div className={classes.headerContainer}>
+        <h1>Saldos y Últimos movimientos</h1>
+        <div className={classes.saldoContainer}>
+          <p className={classes.saldo}>SALDO DISPONIBLE</p>
+          <p className={classes.balance}>{accountInfo.balance}€</p>
         </div>
       </div>
-    )
-}
+      <div className={classes.infoCuentaContainer}>
+        <p>Alias: {accountInfo.name}</p>
+        <p>IBAN: {accountInfo.iban}</p>
+      </div>
+    </div>
+  );
+};
--- src/pages/movement-list/components/movement-list-item.component.tsx
+++ src/pages/movement-list/components/movement-list-item.component.tsx
@@ -8,14 +8,16 @@ interface Props {
 
 export const MovementsListItemComponent : React.FC<Props> = (props) => {
     const {movementItem} = props;
+    const isAmountNegative = movementItem.amount < 0;
+    const isBalanceNegative = movementItem.balance < 0;
 
     return(
         <div className={classes.row}>
-            <span>{movementItem.transaction.toLocaleDateString()}</span>
-            <span>{movementItem.realTransaction.toLocaleDateString()}</span>
-            <span>{movementItem.description}</span>
-            <span>{movementItem.amount}</span>
-            <span>{movementItem.balance}</span>
+            <span className={classes.dataCell}>{movementItem.transaction.toLocaleDateString()}</span>
+            <span className={classes.dataCell}>{movementItem.realTransaction.toLocaleDateString()}</span>
+            <span className={classes.dataCell}>{movementItem.description}</span>
+            <span className={`${classes.dataCell} ${classes.dataNumbers} ${isAmountNegative ? classes.negative : ''}`}>{movementItem.amount} €</span>
+            <span className={`${classes.dataCell} ${classes.dataNumbers} ${isBalanceNegative ? classes.negative : ''}`}>{movementItem.balance} €</span>
         </div>
     )
 }
